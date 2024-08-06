9053f5e393bb2f87594e62af0879b281333dde0d
double0kung <00kung@gmail.com>
2024-08-06 12:41:18 +0000

    feat: added file 11 and 12 and update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,7 @@
-#ifndef MY_HEADER
-#define MY_HEADER
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -9,7 +11,6 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-
 struct binary_tree_s
 {
 	int n;
@@ -24,4 +25,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
-#endif
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
+#endif /* MY_HEADER_H */
