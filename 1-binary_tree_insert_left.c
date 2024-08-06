5ab36dd470855abdb7aa65fe78f9fa2206118248
elxq95 <leongxiqern12@gmail.com>
2024-08-06 09:58:11 +0000

    typo

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@
  * or if parent is NULL
  */
 
-binary_tree_t *binary_tree_innsert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
