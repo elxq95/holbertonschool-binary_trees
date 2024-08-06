0dfbd12fe436b0802e90fe2a5aec29bab12ec9e9
elxq95 <leongxiqern12@gmail.com>
2024-08-06 06:32:03 +0000

    test

---
+++ binary_trees.h
@@ -0,0 +1,25 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
