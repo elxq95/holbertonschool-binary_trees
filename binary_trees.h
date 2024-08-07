7bcc54af7c230388af81b3e3aa133835bdb90ffc
elxq95 <leongxiqern12@gmail.com>
2024-08-07 03:27:31 +0000

    test

--- binary_trees.h
+++ binary_trees.h
@@ -45,4 +45,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 void binary_tree_print(const binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 #endif /* BINARY_TREES_H */
