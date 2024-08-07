0af6c34fe286a0311f7a0439dadc1ac3109d2d06
Vlad <vgozhyi@gmail.com>
2024-08-07 19:16:47 +1000

    Fixes to Q 6-10
    That is all

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,6 +9,8 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 	if (tree->parent)
 	{
 		return (1 + binary_tree_depth(tree->parent));
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,9 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t lheight = 0, rheight = 0;
 
-	if (!tree)
-		return (0);
-	else
+	if (tree)
 	{
 		if (tree->left)
 		{
@@ -36,4 +34,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		else
 			return (rheight);
 	}
+	return (0);
 }
